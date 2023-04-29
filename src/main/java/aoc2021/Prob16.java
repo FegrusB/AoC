741fc03dc9c1ec73e369e0ec03bba60c1fb896b0
FegrusB <fergus@beckerleg.co.uk>
2023-04-29 17:01:51 +0100

    28/04/2023 most of a solve for part 2 working on issue with type ID 0 subpacket count

--- src/main/java/aoc2021/Prob16.java
+++ src/main/java/aoc2021/Prob16.java
@@ -1,7 +1,6 @@
 package aoc2021;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Prob16 {
 
@@ -28,8 +27,89 @@ public class Prob16 {
             versionCount += Integer.parseInt(packet.substring(0,3),2);
         }
 
+        Collections.reverse(packets);
         System.out.println(versionCount);
+        System.out.println(calculate(packets));
     }
+
+    public static Long calculate(ArrayList<String> packets){
+
+        Stack<Long> calcStack = new Stack<>();
+        int i = 0;
+
+        while(packets.size() > 0){
+
+            String curr = packets.get(0);
+            packets.remove(0);
+            String[] currSplit = curr.split(",");
+            if (!Objects.equals(currSplit[1], "100")){i = Integer.parseInt(currSplit[3]);};
+            switch (currSplit[1]) {
+                //type 4: literal
+                case "100" -> {calcStack.add(Long.parseLong(currSplit[2], 2)); }
+                //type 0: sum
+                case "000" -> {
+                    Long x = 0L;
+                    while (i > 0) {
+                        x += calcStack.pop();
+                        i--;
+                    }
+                    calcStack.add(x);
+                }
+                //type 1: product
+                case "001" -> {
+                    Long x = 1L;
+                    while (i > 0) {
+                        x = x * calcStack.pop();
+                        i--;
+                    }
+                    calcStack.add(x);
+                }
+                //type 2: minimuim
+                case "010" -> {
+                    ArrayList<Long> checks = new ArrayList<>();
+                    while (i > 0) {
+                        checks.add(calcStack.pop());
+                        i--;
+                    }
+                    checks.sort(null);
+                    calcStack.add(checks.get(0));
+                }
+                //type 4: maximum
+                case "011" -> {
+                    ArrayList<Long> checks = new ArrayList<>();
+                    while (i > 0) {
+                        checks.add(calcStack.pop());
+                        i--;
+                    }
+                    checks.sort(null);
+                    calcStack.add(checks.get(checks.size() - 1));
+                }
+                //type 5: greater than
+                case "101" -> {
+                    Long x = calcStack.pop();
+                    Long y = calcStack.pop();
+                    calcStack.add((x > y) ? 1L : 0L);
+                }
+                //type 6: less than
+                case "110" -> {
+                    Long x = calcStack.pop();
+                    Long y = calcStack.pop();
+                    calcStack.add((x > y) ? 0L : 1L);
+                }
+                //type 6: equals
+                case "111" -> {
+                    Long x = calcStack.pop();
+                    Long y = calcStack.pop();
+                    calcStack.add((x == y) ? 1L : 0L);
+                }
+            }
+        }
+
+        return calcStack.pop();
+
+    }
+
+
     public static String decodeLiteral(String inString){
 
         boolean finished = false;
@@ -52,8 +132,10 @@ public class Prob16 {
 
         boolean boundLoop = false;
         int x = 0;
+        int i = 0;
 
         while ( !(inString.length() < 11 )&& !boundLoop) {
+            i++;
             if (numLoops != 0){ boundLoop = x < numLoops - 1; }
 
             //decode and remove version + id
@@ -80,13 +162,19 @@ public class Prob16 {
                     //parse length from bin as dec, substring based on result
                     length = Integer.parseInt(inString.substring(1, 16), 2);
                     String body = inString.substring(16,length+16);
+                    outString = outString + inString.charAt(0);
+                    inString = inString.substring(length+16);
+                    packets.add(outString + ",");
 
-                    packets.add(outString + inString.charAt(0) + "," + length);
+                    length = Integer.parseInt(decode(packets,body,0));
 
-                    decode(packets,body,0);
-
-                    int cutLength = length+16;
-                    inString = inString.substring(cutLength);
+                    boolean set = false;
+                    int y = packets.size() - 1;
+                    while(!set) {
+                        String s = packets.get(y);
+                        if(s.charAt(s.length() - 1 ) == ','){packets.set(y, s + length); set = true; }
+                        y--;
+                    }
 
                 } else {
                     length = Integer.parseInt(inString.substring(1,12),2);
@@ -96,6 +184,8 @@ public class Prob16 {
             }
         }
 
+        if(numLoops == 0){
+            inString = String.valueOf(i);}
         return inString;
 
     }

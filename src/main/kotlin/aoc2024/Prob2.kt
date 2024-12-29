d7c9837202b73003c152c9382771d90d781a6bfc
FegrusB <fergus@beckerleg.co.uk>
2024-12-29 20:19:37 +0000

    part 1 works, prat 2 bug

---
+++ src/main/kotlin/aoc2024/Prob2.kt
@@ -0,0 +1,46 @@
+package aoc2024
+
+import Common.GetScanner
+import kotlin.math.abs
+
+fun main() {
+    val records: MutableList<List<Int>> = mutableListOf()
+
+    GetScanner.getStream(2024,"Day-2").bufferedReader().useLines { lines -> lines.forEach { records.add(it.split(" ").map { it.toInt() }) } }
+
+    var count = 0
+    records.forEach { if(checkSafe(it,false)){count++} }
+    println(count)
+    count = 0
+    records.forEach { if(checkSafe(it,true)){count++} }
+    println(count)
+
+}
+
+fun checkSafe(record: List<Int>, part2: Boolean): Boolean {
+    var safe = true
+    var lastInt = record.first()
+    var i = 1
+    var growing = false
+    var failedI = -1
+
+    if( record[1] > lastInt){ growing = true}
+    while(safe && i < record.size ){
+
+        val abs = abs(record[i] - lastInt)
+        if(abs < 1 ||  3 < abs){safe = false }
+
+        if( record[i] <= lastInt && growing ){ safe = false }
+        else if(record[i] >= lastInt && !growing ){safe = false }
+
+        if(!safe){failedI = i}
+        lastInt = record[i]
+        i++
+
+    }
+
+    if( (!safe)&& part2){
+        safe = checkSafe(record.filterIndexed { i, _ -> i != failedI }, false) }
+
+    return safe
+}

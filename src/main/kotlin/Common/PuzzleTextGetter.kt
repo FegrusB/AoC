f828ba92032bd77527965e6d6660ffe3eb9838a3
FegrusB <fergus@beckerleg.co.uk>
2024-12-29 16:07:00 +0000

    first draft for new auto-puzzle text feature

--- src/main/kotlin/Common/PuzzleTextGetter.kt
+++ src/main/kotlin/Common/PuzzleTextGetter.kt
@@ -7,18 +7,32 @@ import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.Cookie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
 
-suspend fun getPuzzleText(): HttpResponse {
+suspend fun getPuzzleText(year: Int, day: Int = 30): String {
     val client = HttpClient(CIO) {
         install(HttpCookies) {
             storage = ConstantCookiesStorage(Cookie(name = "session", value = GetScanner.getCookie(), domain = "adventofcode.com"))
         }
     }
-    val response: HttpResponse = client.get("https://adventofcode.com/2024/day/1/input")
-    return response
+    val test = "https://adventofcode.com/$year/day/$day/input"
+    println(test)
+    val response: HttpResponse = client.get("https://adventofcode.com/$year/day/$day/input")
+    return response.bodyAsText()
 }
 
-fun main() = kotlinx.coroutines.runBlocking {
-    println(getPuzzleText().bodyAsText())
+fun main( ) = kotlinx.coroutines.runBlocking {
+    val year = 2024
+    val day = 3
+
+    val f = File("src/main/resources/PuzzleText/$year/Day-$day.txt")
+    withContext(Dispatchers.IO) {
+        f.createNewFile()  // This is the answer to the question
+        f.printWriter().use { out ->
+            out.println(getPuzzleText(year,day))
+        }
+    }
 }
 
